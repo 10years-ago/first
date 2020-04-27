861f4f6a0700ea275bdc91d27fcd5aa196aa00ba
10years-ago <414578531@qq.com>
2020-04-27 20:17:22 +0800

    git  commit -m

--- vue.config.js
+++ vue.config.js
@@ -1,30 +1,4 @@
-/*  module.exports = {
-  configureWebpack: {
-    resolve: {
-      alias:{
-        '@':'srcalskdjas;ldjl',
-        'assets': '@/assets',
-        'common': '@/common',
-        'components': '@/components',
-        'network': '@/network',
-        'views': '@/views'
-      }
-    }
-  }
-}  坑！！！！只能给webstorm使用*/
-
 module.exports = {
-  // devServer:{
-  //   proxy: {
-  //     '/yes':{
-  //       target:"http://127.0.0.1",
-  //       changeOrigin: true,
-  //       pathRewrite:{
-  //         '^/yes':'/'
-  //       }
-  //     }
-  //   }
-  // },
   configureWebpack: {
     resolve: {
       alias:{
