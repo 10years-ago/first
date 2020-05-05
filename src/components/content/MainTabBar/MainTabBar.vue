c4bfd4bfaa5f472fa354235723a570599253fe87
10years-ago <414578531@qq.com>
2020-05-05 18:19:50 +0800

    master

--- src/components/content/MainTabBar/MainTabBar.vue
+++ src/components/content/MainTabBar/MainTabBar.vue
@@ -1,23 +1,23 @@
 <template>
     <tab-bar>
         <tab-bar-item path="/home" activeColor="blue">
-          <img slot="item-icon" src="~assets/img/tabbar/tab1.jpg" alt="">
-          <img slot="item-iconA" src="~assets/img/tabbar/tab1a.jpg" alt="">
+          <img slot="item-icon" src="~assets/img/tabbar/tab1.png" alt="">
+          <img slot="item-iconA" src="~assets/img/tabbar/tab1a.png" alt="">
           <div slot="item-text">首页</div>
         </tab-bar-item>
         <tab-bar-item path="/category" activeColor="blue">
-          <img slot="item-icon" src="~assets/img/tabbar/tab2.jpg" alt="">
-          <img slot="item-iconA" src="~assets/img/tabbar/tab2a.jpg" alt="">
+          <img slot="item-icon" src="~assets/img/tabbar/tab2.png" alt="">
+          <img slot="item-iconA" src="~assets/img/tabbar/tab2a.png" alt="">
           <div slot="item-text">分类</div>
         </tab-bar-item>
         <tab-bar-item path="/cart" activeColor="blue">
-          <img slot="item-icon" src="~assets/img/tabbar/tab3.jpg" alt="">
-          <img slot="item-iconA" src="~assets/img/tabbar/tab3a.jpg" alt="">
+          <img slot="item-icon" src="~assets/img/tabbar/tab3.png" alt="">
+          <img slot="item-iconA" src="~assets/img/tabbar/tab3a.png" alt="">
           <div slot="item-text">购物车</div>
         </tab-bar-item>
         <tab-bar-item path="/profile" activeColor="blue">
-          <img slot="item-icon" src="~assets/img/tabbar/tab4.jpg" alt="">
-          <img slot="item-iconA" src="~assets/img/tabbar/tab4a.jpg" alt="">
+          <img slot="item-icon" src="~assets/img/tabbar/tab4.png" alt="">
+          <img slot="item-iconA" src="~assets/img/tabbar/tab4a.png" alt="">
           <div slot="item-text">我的</div>
         </tab-bar-item>
     </tab-bar>
